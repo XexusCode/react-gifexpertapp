5b158e23a162694af8a440648901ee4e22623272
XexusCode <xexuszero@gmail.com>
2020-09-14 13:09:55 +0200

    docs agregado

---
+++ docs/precache-manifest.594c82f73a31602113fa4f51f5457af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d491c04c2172f29b46a2e460688b6815",
+    "url": "/index.html"
+  },
+  {
+    "revision": "770d0e1d09dd4bdeae72",
+    "url": "/static/css/main.528cf28f.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "770d0e1d09dd4bdeae72",
+    "url": "/static/js/main.ddc1027a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
